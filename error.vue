6f1578d65b04fe13d0832849852d7cf82f025f1f
Laura Simoni <laura.simoni@gmail.com>
2023-12-27 10:57:20 +0000

    server-side error handling with clearError

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLayout>
+    <div v-if="error.statusCode === 404" class="prose">
+      <h1>404</h1>
+      <p>I guess that page doesn't exist.</p>
+    </div>
+    <div v-else class="prose">
+      <h1>Dang</h1>
+      <p>It looks like something broke</p>
+      <p>Sorry about that.</p>
+    </div>
+    <p><strong>{{error.message}}</strong></p>
+    <button class="rounded text-white font-bold py-2 px-4 my-8 cursor-pointer bg-gray-500" @click="handleError()">Go back to Course</button>
+  </NuxtLayout>
+</template>
+<script setup>
+const error = useError();
+
+const handleError = () => {
+  clearError({redirect:'/course'})
+}
+</script>
